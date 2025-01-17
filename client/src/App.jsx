f3e4d993447e0e3fdb42a5ca0352dbcbdb45d49e
PatelPratik39 <ptl.pratik717@gmail.com>
2025-01-16 18:35:05 -0600

    fronend and backend merged through concurrency package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <div >
-        <h1>Hello Chat GPT</h1>
-      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
